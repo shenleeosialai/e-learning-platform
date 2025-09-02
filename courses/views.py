dd562e0cf80fbe12e8afec7a068478febbe5a291
shen lee <shenleekhalid@gmail.com>
2025-09-02 15:22:32 +0300

    using mixins for class based views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from . models import Course
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, \
+    UpdateView, DeleteView
 
-# Create your views here.
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class ownerCourseMixin(OwnerMixin, ListView):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(ownerCourseMixin):
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseEditMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
