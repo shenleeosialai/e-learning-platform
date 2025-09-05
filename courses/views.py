c2231a3104db3984d94e66f01bf9b5933762051c
shen lee <shenleekhalid@gmail.com>
2025-09-05 19:48:21 +0300

    managing modules

--- courses/views.py
+++ courses/views.py
@@ -74,3 +74,13 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             return redirect('manage_course_list')
         return self.render_to_response({'course': self.course,
                                         'formset': formset})
+
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(
+            Module, id=module_id, course__owner=request.user
+            )
+        return self.render_to_response({'module': module})
