b7abb47e5e917471efc9fc137dedca49f1bc9756
shen lee <shenleekhalid@gmail.com>
2025-09-21 23:59:40 +0300

    w

--- educa/settings.py
+++ educa/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "embed_video",
     "debug_toolbar",
     "redisboard",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,10 @@ INTERNAL_IPS = [
 CACHE_MIDDLEWARE_ALIAS = "default"
 CACHE_MIDDLEWARE_SECONDS = 60 * 15
 CACHE_MIDDLEWARE_KEY_PREFIX = "educa"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        ]
+}
