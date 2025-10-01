53e26428871ab4a9363da77ed4c716ced7f45699
shen lee <shenleekhalid@gmail.com>
2025-10-01 16:29:43 +0300

    setting up channel layer with redis

--- educa/settings.py
+++ educa/settings.py
@@ -160,3 +160,12 @@ REST_FRAMEWORK = {
 }
 
 ASGI_APPLICATION = 'educa.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
