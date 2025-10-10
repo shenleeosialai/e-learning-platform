479c853c2e2177cdd7fab30666b9b3f2413d6b9c
shen lee <shenleekhalid@gmail.com>
2025-10-10 16:04:40 +0300

    including daphne in the nginx configuration

--- educa/asgi.py
+++ educa/asgi.py
@@ -2,24 +2,28 @@
 ASGI config for educa project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
+django.setup()
+
+from chat.routing import websocket_urlpatterns  # Safe to import now
+
 django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
