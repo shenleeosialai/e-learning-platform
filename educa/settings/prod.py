a48f7f63b54127200c6847e5161e1b8d678f6791
shen lee <shenleekhalid@gmail.com>
2025-10-11 23:39:46 +0300

    creating subdomain middleware

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -8,7 +8,7 @@ ADMINS = [
     ('shen', 'shenleekhalid@gmail.com'),
 ]
 
-ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+ALLOWED_HOSTS = ['.educaproject.com']
 
 DATABASES = {
     'default': {
